04d8f185368b8aecf09a110cb97a8894471da4df
eggeek <eggeek.me@gmail.com>
2023-10-22 05:26:23 +1100

    fix cmp-keymap

--- lua/user/cmp-core.lua
+++ lua/user/cmp-core.lua
@@ -226,36 +226,6 @@ M.config = function()
     },
     sources = {
       {
-        name = "copilot",
-        -- keyword_length = 0,
-        max_item_count = 3,
-        trigger_characters = {
-          {
-            ".",
-            ":",
-            "(",
-            "'",
-            '"',
-            "[",
-            ",",
-            "#",
-            "*",
-            "@",
-            "|",
-            "=",
-            "-",
-            "{",
-            "/",
-            "\\",
-            "+",
-            "?",
-            " ",
-            -- "\t",
-            -- "\n",
-          },
-        },
-      },
-      {
         name = "nvim_lsp",
         entry_filter = function(entry, ctx)
           local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
@@ -268,22 +238,17 @@ M.config = function()
 
       { name = "path" },
       { name = "luasnip" },
-      -- { name = "cmp_tabnine" },
       { name = "nvim_lua" },
       { name = "buffer" },
-      -- { name = "calc" },
-      -- { name = "emoji" },
       { name = "treesitter" },
-      { name = "crates" },
-      { name = "tmux" },
+      -- { name = "crates" },
+      -- { name = "tmux" },
     },
-    mapping = cmp_mapping.preset.insert {
+    mapping = {
       ["<C-k>"] = cmp_mapping(cmp_mapping.select_prev_item(), { "i", "c" }),
       ["<C-j>"] = cmp_mapping(cmp_mapping.select_next_item(), { "i", "c" }),
-      ["<Down>"] = cmp_mapping(cmp_mapping.select_next_item { behavior = SelectBehavior.Select }, { "i" }),
-      ["<Up>"] = cmp_mapping(cmp_mapping.select_prev_item { behavior = SelectBehavior.Select }, { "i" }),
-      ["<C-d>"] = cmp_mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp_mapping.scroll_docs(4),
+      ["<M-k>"] = cmp_mapping.scroll_docs(-4),
+      ["<M-j>"] = cmp_mapping.scroll_docs(4),
       ["<C-y>"] = cmp_mapping {
         i = cmp_mapping.confirm { behavior = ConfirmBehavior.Replace, select = false },
         c = function(fallback)
@@ -294,7 +259,7 @@ M.config = function()
           end
         end,
       },
-      ["<Tab>"] = cmp_mapping(function(fallback)
+      ["<C-l>"] = cmp_mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_locally_jumpable() then
@@ -308,37 +273,18 @@ M.config = function()
           fallback()
         end
       end, { "i", "s" }),
-      ["<S-Tab>"] = cmp_mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
       ["<C-Space>"] = cmp_mapping.complete(),
       ["<C-e>"] = cmp_mapping.abort(),
       ["<CR>"] = cmp_mapping(function(fallback)
         if cmp.visible() then
-          local confirm_opts = vim.deepcopy(M.cmp.confirm_opts) -- avoid mutating the original opts below
-          local is_insert_mode = function()
-            return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
+          if cmp.get_selected_entry() then
+            cmp.confirm { behavior = ConfirmBehavior.Replace, select = false }
+          else
+            fallback()
           end
-          if is_insert_mode() then -- prevent overwriting brackets
-            confirm_opts.behavior = ConfirmBehavior.Insert
-          end
-          local entry = cmp.get_selected_entry()
-          local is_copilot = entry and entry.source.name == "copilot"
-          if is_copilot then
-            confirm_opts.behavior = ConfirmBehavior.Replace
-            confirm_opts.select = true
-          end
-          if cmp.confirm(confirm_opts) then
-            return -- success, exit early
-          end
+        else
+          fallback() -- if not exited early, always fallback
         end
-        fallback() -- if not exited early, always fallback
       end),
     },
     cmdline = {
