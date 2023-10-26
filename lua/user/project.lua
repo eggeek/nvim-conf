9a3dae17053a4788b6ec65dcb7a5f1b66b47b503
eggeek <eggeek.me@gmail.com>
2023-10-26 21:47:45 +1100

    set project to manual

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -6,7 +6,7 @@ local M = {
 function M.config()
   require("project_nvim").setup {
     on_config_done = nil,
-    manual_mode = false,
+    manual_mode = true,
     detection_methods = { "pattern" },
     patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml" },
     ignore_lsp = {},
