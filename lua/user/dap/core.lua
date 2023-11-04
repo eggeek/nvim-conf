ab4d67fab8797b94b53150ba28cb0144b950eee6
eggeek <eggeek.me@gmail.com>
2023-11-05 03:46:51 +1100

    fix dap map/unmap

--- lua/user/dap/core.lua
+++ lua/user/dap/core.lua
@@ -5,12 +5,11 @@ local dapui = require 'dapui'
 local widgets = require 'dap.ui.widgets'
 local keymaps = {
   ['n'] = { -- normal mode
-    ["<C-n>"]      = dap.step_over,
-    ["<C-s>"]      = dap.step_into,
-    ["<C-c>"]      = dap.continue,
-    ["<C-g>"]      = dap.step_out,
-    ["<C-r>"]      = dap.run_to_cursor,
-    ["K"]          = widgets.hover,
+    ["<C-N>"]      = dap.step_over,
+    ["<C-S>"]      = dap.step_into,
+    ["<C-C>"]      = dap.continue,
+    ["<C-G>"]      = dap.step_out,
+    ["<C-R>"]      = dap.run_to_cursor,
     ["<leader>dx"] = function()
       dap.terminate({}, {}, M.RmDbgKeyMapping)
       dapui.close()
@@ -34,17 +33,50 @@ local function _clear()
 end
 
 local map = function(mode, lhs, rhs)
+  -- not nil -> (mode, lhs, rhs) has been mapped before, e.g., restart
+  if original[mode][lhs] ~= nil then
+    return
+  end
   local exists = vim.api.nvim_get_keymap(mode)
   original[mode][lhs] = vim.tbl_filter(function(v)
-    return v.lhs == lhs
+    return vim.api.nvim_replace_termcodes(v.lhs, true, false, true) ==
+        vim.api.nvim_replace_termcodes(lhs, true, false, true)
   end, exists)[1] or true
 
   vim.keymap.set(mode, lhs, rhs)
+  -- print("mode:", mode, "lhs:", lhs, vim.inspect(original[mode][lhs]))
+end
+
+local unmap = function(mode, lhs, item)
+  -- remove from original
+  original[mode][lhs] = nil
+
+  -- no original map, directly unmap
+  if item == true then
+    vim.keymap.del(mode, lhs)
+  else -- map to original
+    -- rhs is '' if map to lua callback function
+    local rhs      = item.rhs or ''
+    item.lhs       = nil
+    item.lhsraw    = nil
+    item.lhsrawalt = nil
+    item.rhs       = nil
+    item.mode      = nil
+    item.sid       = nil
+    item.lnum      = nil
+    -- remove zero value options, e.g., {expr=0}
+    for key, val in pairs(item) do
+      if val == 0 then
+        item[key] = nil
+      end
+    end
+    -- print("mode:", mode, "lhs:", lhs, "rhs:", rhs, "v:", vim.inspect(item))
+    vim.keymap.set(mode, lhs, rhs, item)
+  end
 end
 
 function M.DbgKeyMapping()
   notify("Load Debug Keymapping", "info")
-  _clear()
   for mode, mappings in pairs(keymaps) do
     for lhs, rhs in pairs(mappings) do
       map(mode, lhs, rhs)
@@ -55,39 +87,19 @@ end
 function M.RmDbgKeyMapping()
   notify("Remove Debug Keymapping", "info")
   for mode, mapping in pairs(original) do
+    -- print("mapping:", vim.inspect(mapping))
     for lhs, v in pairs(mapping) do
-      if v == true then
-        vim.keymap.del(mode, lhs)
-      else
-        local rhs = v.rhs
-        v.lhs       = nil
-        v.lhsraw    = nil
-        v.lhsrawalt = nil
-        v.rhs       = nil
-        v.mode      = nil
-        v.sid       = nil
-        v.lnum      = nil
-        for key, val in pairs(v) do
-          if val == 0 then
-            v[key] = nil
-          end
-        end
-        vim.keymap.set(mode, lhs, rhs, v)
-      end
+      unmap(mode, lhs, v)
     end
   end
-  _clear()
 end
 
 function M.common_dap_keymap()
+  -- auto open dapui, manually close 
   dap.listeners.after.event_initialized["dapui_config"] = function()
     notify("DBG Sesstion Start", "info")
     M.DbgKeyMapping()
     dapui.open()
-  end
-
-  dap.listeners.before.event_terminated["dapui_config"] = function()
-    notify("DBG Sesstion Terminated", "info")
   end
 
   vim.keymap.set('n', '<M-b>', dap.toggle_breakpoint)
@@ -100,7 +112,6 @@ function M.common_dap_conf()
 end
 
 M.setup = function()
-
   local conf = require "user.dap.config"
 
   vim.fn.sign_define("DapBreakpoint", conf.breakpoint)
