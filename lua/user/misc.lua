fa2f0db49bdb232198bad162b6b3473b0285eda0
eggeek <eggeek.me@gmail.com>
2024-06-16 12:07:31 +0800

    leap replace flash

--- lua/user/misc.lua
+++ lua/user/misc.lua
@@ -33,18 +33,14 @@ local M = {
     end
   },
   {
-    "folke/flash.nvim",
+    "ggandor/leap.nvim",
     event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    dependencies = {
+      "tpope/vim-repeat"
     },
+    config = function ()
+      require('leap').create_default_mappings()
+    end
   },
   { -- predefined stubs for pyright
     "microsoft/python-type-stubs",
